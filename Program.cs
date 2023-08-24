afae2be309b97df6051a87efbd56e5cf5bcd1a3d
NewRound <86642886+NewRound@users.noreply.github.com>
2023-08-24 10:11:26 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AlgorithmKata
+{
+    /* 2023.08.24
+    짝수와 홀수
+    
+
+     */
+
+
+
+    internal class Program
+    {
+        // 짝수와 홀수
+        public static string solution(int num)
+        {
+            if (num % 2 == 0)
+                return "Even";
+            else
+                return "Odd";
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
